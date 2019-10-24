9753038ec1eaa49b3bde58854d3c194e417d9fd1
wind2esg <wind2esg@gmail.com>
2019-10-24 13:37:04 +0800

    v1.2.2 nested swiper, that is SCarousel component pagination added. Svideo component classified into inline and not inline

--- src/Projects/ProjectsComponents.tsx
+++ src/Projects/ProjectsComponents.tsx
@@ -5,12 +5,21 @@
  * @date 20191016
  * 
  * your components here
+ * 
+ * #SImg
+ * #SVideoInline
+ * #SVideo
+ * #SCarousel
  */
 
 import React from 'react';
 import './ProjectsComponents.css';
 import Swiper from 'swiper';
 
+/**
+ *  #SImg
+ * 
+ */ 
 interface SImgProps{
     src: string;
     alt?: string;
@@ -24,6 +33,10 @@ export class SImg extends React.Component<SImgProps>{
     }
 }
 
+/**
+ *  #SVideoInline
+ *  #SVideo
+ */ 
 interface SVideoProps{
     poster?: string;
     src: string;
@@ -34,7 +47,7 @@ export class SVideo extends React.Component<SVideoProps>{
     render(){
         return (
             <>
-                <video autoPlay controls playsInline webkit-playsinline="true" poster={this.props.poster}>
+                <video autoPlay controls poster={this.props.poster}>
                     <source src={this.props.src} type={this.props.type ? this.props.type : "video/mp4" } ></source>
                 </video>
             </>
@@ -42,14 +55,43 @@ export class SVideo extends React.Component<SVideoProps>{
     }
 }
 
+export class SVideoInline extends React.Component<SVideoProps>{
+    render(){
+        return (
+            <>
+                <video autoPlay controls playsInline webkit-playsinline poster={this.props.poster}>
+                    <source src={this.props.src} type={this.props.type ? this.props.type : "video/mp4" } ></source>
+                </video>
+            </>
+        );
+    }
+}
+
+/**
+ *  #SCarousel
+ * 
+ */ 
 interface SCarouselProps{
     srcs: Array<string>;
     carouselClassName: string;
+    pagination?: boolean;
+    paginationClassName?: string;
 }
 
 export class SCarousel extends React.Component<SCarouselProps>{
     componentDidMount(){
-        new Swiper(`.${this.props.carouselClassName}`);
+        new Swiper(`.${this.props.carouselClassName}`, 
+            this.props.pagination
+            ?
+            {
+                pagination:{
+                    el: `.${this.props.paginationClassName}`
+                }
+            }
+            :
+            {}
+        );
+        
     }
 
     render(){
@@ -62,6 +104,7 @@ export class SCarousel extends React.Component<SCarouselProps>{
                         </div>
                     ))}
                 </div>
+                { this.props.pagination && <div className={this.props.paginationClassName}></div> }
             </div>
         );
     }

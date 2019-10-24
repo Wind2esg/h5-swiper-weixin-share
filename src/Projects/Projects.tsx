6b7bb27ac60af8708d1b8858f7c68b8faec8eff4
wind2esg <wind2esg@gmail.com>
2019-10-24 13:55:22 +0800

    projects filled

--- src/Projects/Projects.tsx
+++ src/Projects/Projects.tsx
@@ -16,38 +16,37 @@ import { SImg, SVideo, SCarousel } from './ProjectsComponents';
 let slides: Array<Array<any>> = [
     // slide 1
     [
-        <SCarousel carouselClassName="carousel" srcs={["", "","",""]} />,
-        <SVideo src="" poster="" />,
-        <SVideo src="" poster="" />,
-        <SVideo src="" poster="" />,
-        <SVideo src="" poster="" />,
-        <SVideo src="" poster="" />,
-        <SVideo src="" poster="" />,
-        <SImg src="" />,
-        <SImg src="" />,
-        <SImg src="" />,
-        <SImg src="" />
+        <SImg src="https://res.manzhiyan.com/BrandDept/h5_191024/1_1.jpg" />,
+        <SCarousel carouselClassName="carousel" 
+            pagination={true} 
+            paginationClassName="swiper-pagination" 
+            srcs={[
+                "https://res.manzhiyan.com/BrandDept/h5_191024/1_2c1.jpg",
+                "https://res.manzhiyan.com/BrandDept/h5_191024/1_2c2.jpg",
+                "https://res.manzhiyan.com/BrandDept/h5_191024/1_2c3.jpg",
+                "https://res.manzhiyan.com/BrandDept/h5_191024/1_2c4.jpg"
+            ]} />,
+        <SImg src="https://res.manzhiyan.com/BrandDept/h5_191024/1_3.jpg" />
     ],
     // slide 2
     [
-        <SVideo src="" poster="" />,
-        <SVideo src="" poster="" />,
-        <SImg src="" />
+        <SImg src="https://res.manzhiyan.com/BrandDept/h5_191024/2_1.jpg" />,
+        <SVideo src="https://res.manzhiyan.com/BrandDept/h5_191024/2_v1.mp4" poster="https://res.manzhiyan.com/BrandDept/h5_191024/2_p1.jpg" />,
+        <SImg src="https://res.manzhiyan.com/BrandDept/h5_191024/2_3.jpg" />
     ],
     // slide 3
     [
-        // <SImg src="" />,
-        // <SVideo src="" />,
+        <SImg src="https://res.manzhiyan.com/BrandDept/h5_191024/3_1.jpg" />,
     ]
     // slide ...
 ];
 
 export let projects = {} as AppState;
 // params for wechat share centre service
-projects.wechatServiceUrl = "";
-projects.wechatServiceParams = {};
+projects.wechatServiceUrl = "http://hh.manzhiyan.com/services/jssdkservice.php";
+projects.wechatServiceParams = {token:"superm", url: (window as any).location.href };
 // wechat share link params
-projects.debug = true;
+projects.debug = false;
 projects.shareTitle = "";
 projects.shareDescription = "";
 projects.shareImgSrc = "";
